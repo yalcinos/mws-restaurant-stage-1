e19f337de0866785399af94d576189f970716d1c
yalcintatar <yalcintatar41@gmail.com>
2018-05-04 21:46:59 +0300

    feat:Add service worker for offline usage

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch', function(event) {
-  console.log(event.request);
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
