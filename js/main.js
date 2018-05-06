bc0f10199eb9dbef8fa01aeaf6c9d20095dc890e
yalcintatar <yalcintatar41@gmail.com>
2018-05-06 23:40:09 +0300

    refactor:Modify 'h' tags from some css parts

--- js/main.js
+++ js/main.js
@@ -167,7 +167,7 @@ createRestaurantHTML = (restaurant) => {
   picture.append(image);
   
   
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
