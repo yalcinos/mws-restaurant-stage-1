4a0e493101d96f05025bbdcddcfede75b99c6b26
yalcintatar <yalcintatar41@gmail.com>
2018-06-12 18:44:25 +0300

    feat:Add babel-core

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
 var babel= require('gulp-babel')
-var sourcmaps = require('gulp-sourcemaps')
 var imagemin = require('gulp-imagemin')
 var pngquant = require('imagemin-pngquant')
 
@@ -10,18 +9,14 @@ var pngquant = require('imagemin-pngquant')
 
 gulp.task('default', function() {
 	//Asagıdakı kod : Eger sass dosyanın içerisinde herhangi bir .scss uzantılı dosyada degisiklik olursa. styles taskini çalıştır.
-	
-	gulp.watch('js/**/*.js', ['lint'])
-	gulp.watch('/index.js', ['copy-html'])
-	
+
+	console.log('Success!');
 })
 gulp.task('scripts-dist',function(){
 	gulp.src('js/**/*.js')
-		.pipe(sourcmaps.init())
 		.pipe(babel())
 		//js dosyasının içerisindeki js fileları birleştirip all.js e dosyasını yaratıp içine atar.
 		.pipe(concat('all.js'))
 		.pipe(uglify())
-		.pipe(sourcmaps.write())
 		.pipe(gulp.dest('dist/js'))
 })
