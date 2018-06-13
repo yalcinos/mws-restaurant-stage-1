99b308bbc005192b8ec8e7d4f77ccee11a72fb9f
yalcintatar <yalcintatar41@gmail.com>
2018-06-13 19:49:22 +0300

    feat:Implement idb-romise package to idb.js which has basic idb implementation

--- js/idb.js
+++ js/idb.js
@@ -1,29 +1,23 @@
-var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+import idb from 'idb';
 
-var open = indexedDB.open('test-db',1);
-
-// Create the schema
-open.onupgradeneeded = function() {
-    var db = open.result;
-    var store = db.createObjectStore("RestaurantStore", {keyPath:"id"});
-};
-
-open.onsuccess = function CreateDB() {
-		var db = open.result;
-      	 
     // Start a new transaction
-      fetch("http://localhost:1337/restaurants/")
-      .then(function(response){
+idb.open('keyval-store', 1, upgradeDB => {
+  switch (upgradeDB.oldVersion) {
+    case 0:
+      upgradeDB.createObjectStore('keyval', {keyPath:'id'});
+  }
+}).then(db => {
+        fetch("http://localhost:1337/restaurants/")
+        .then(function(response){
          return response.json()
-      })
-      .then(function(jsonData){
-      	 var tx = db.transaction("RestaurantStore", "readwrite");
-   		 var store = tx.objectStore("RestaurantStore");
-   		 console.log(jsonData);
-      	for(var i=0; i<jsonData.length; i++){
-      		store.put(jsonData[i]);
-      	}
-      	return store.getAll();
-      });
-}
- 
+        })
+        .then(function(jsonData){
+          var tx = db.transaction("keyval", "readwrite");
+          var store = tx.objectStore("keyval");
+          console.log(jsonData);
+          for(var i=0; i<jsonData.length; i++){
+            store.put(jsonData[i]);
+          }
+            return tx.complete && return store.getAll();
+        });
+}).then(() => console.log("Done!"));
