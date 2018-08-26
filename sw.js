9433eb34d0a04ecca7134e94242dfcfe9fda88e6
yalcintatar <yalcintatar41@gmail.com>
2018-08-26 23:48:22 +0300

    feat:Add Background-sync skeleton.

--- sw.js
+++ sw.js
@@ -1,14 +1,52 @@
 
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
       // Registration was successful
+      //Background-sync
+      if('sync' in registration){
+      	//Doit
+      	const rest_id = parseInt(getParameterByName('id'));
+      	//const form =document.getElementsByTagNama("form");
+    	const uname = document.getElementById("uname").value;
+     	const rate = parseInt(document.getElementById("rate").value);
+     	const comment = document.getElementById("subject").value;
+     	const rewButton = document.getElementById("submitReview");
+ 		rewButton.addEventListener("click",function(event){
+    	//event.preventDefault();
+        const reviewData = {
+     		"restarant_id": rest_id,
+      		"name": uname,
+      		"rating": rate,
+      		"comments": comment
+    	};
+    	console.log('DSADSA:', reviewData);
+    	idb.open('messages', 1, function(upgradeDb) {
+      upgradeDb.createObjectStore('outbox', { autoIncrement : true, keyPath: 'id' });
+    }).then(function(db){
+    	var tx=db.transaction('outbox', 'readwrite');
+    	return tx.objectStore('outbox').put(reviewData);
+    }).then(function(){
+    	//do some stuff
+    	return registration.sync.register('outbox');
+    }).catch(function(err){
+    	console.log(err);
+    	//form.submit();
+    });
+
+    console.log('Kudur:', reviewData);
+    });
+      }
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
-  });
+   
+  }
+
+  );
 }
 
 var staticCacheName='restaurant-v1';
@@ -71,5 +109,4 @@ self.addEventListener('activate',function(event){
 	
 	
 });
-
 
