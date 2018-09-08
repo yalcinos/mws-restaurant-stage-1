4e13cc697455a29a762b954218e2c22db1841e92
yalcintatar <yalcintatar41@gmail.com>
2018-09-08 17:52:12 +0300

    refactor:Amendment of code syntax

--- js/idb-restaurant.js
+++ js/idb-restaurant.js
@@ -1,10 +1,8 @@
 import idb from 'idb';
 
-///////////////////
-
-//INITIALIZE idp objects
-
-////////////////////
+/**
+  Initialize idp object for offline storage.
+*/
   const dbPromised = idb.open('review-store', 1, upgradeDB => {
   switch (upgradeDB.oldVersion) {
     case 0:
@@ -18,68 +16,51 @@ import idb from 'idb';
 
   }
 });
-  const dbPromisedOutBox = idb.open('outbox', 1, upgradeDB => {
-  switch (upgradeDB.oldVersion) {
-    case 0:
-      upgradeDB.createObjectStore('outitems', {keyPath:'id'});
-      
-  }
-});
-//END OF INITIALIZE
-
-
-////////////////
-
-//ADD REVIEW DATA TO INDEXDB FOR OFFLINE USAGE
-
-/////////////
+/**
+    Add review data to IndexDB for offline usage.
+*/
   dbPromised.then(db => {
         fetch("http://localhost:1337/reviews/")
-        .then(function(response){
-         return response.json()
+          .then(function(response){
+             return response.json()
         })
-        .then(function(jsonData){
-          var tx = db.transaction("reviews", "readwrite");
-          var store = tx.objectStore("reviews");
-          console.log(jsonData);
-          for(var i=0; i<jsonData.length; i++){
-            store.put(jsonData[i]);
-          }
+          .then(function(jsonData){
+            var tx = db.transaction("reviews", "readwrite");
+            var store = tx.objectStore("reviews");
+            console.log(jsonData);
+            for(var i=0; i<jsonData.length; i++){
+              store.put(jsonData[i]);
+            }
             return tx.complete && store.getAll();
-        });
-}).then(result => {console.log("Done!")});
-
-//Get Data from indexDB
-  dbPromised.then(db => {return db.transaction("reviews")
-                        .objectStore("reviews").get(1);
-                      }).then(obj => console.log(obj.name,obj.is_favorite,obj.neighborhood));
-
-
-//When user offline,get review from indexDB for offline usage.
+          });
+  });
+/**
+  When user offline,get review from indexDB for offline usage.
+*/
 if(window.navigator.onLine){
   console.log('online!');
 
 }else{
   console.log('offline');
+
 //Post data to page when user offline.
   dbPromised.then(db =>{
     var tx = db.transaction("reviews","readonly");
     var store = tx.objectStore("reviews");
     return store.getAll();
-  
   }).then(data => {
     const reviewForRest = data.filter(res => parseInt(res.restaurant_id) == getParameterByName('id'));
     const ul = document.getElementById('reviews-list');
     console.log('indsad:' ,reviewForRest);
-      fillReviewsHTML(reviewForRest)});
+    fillReviewsHTML(reviewForRest)
+    });
 }
-/////////////////////////////////////////7
+/**
+  *Starts Restaurant Detail Page.
+  *Add restaurant data when user offline to indexDB.
+  *Get and put restaurant data to /restaurant route.
+*/
 
-//---RESTAURANT-DETAİL----
-
-//////////////////////////////////////////
-//add restaurant data when user offline to ındexDB.
-//Get idb for restaurant details of restaurant_info page
 if(window.navigator.onLine){
   console.log('online!');
     dbPromisedRestaurantDetail.then(db => {
@@ -106,7 +87,7 @@ if(window.navigator.onLine){
     var tx = db.transaction("items","readonly");
     var store = tx.objectStore("items");
     return store.getAll();
-  
+    
   }).then(data => {
     const IndexedRestData = data.find(res => parseInt(res.id) == getParameterByName('id'));
     console.log('ABD:', IndexedRestData);
@@ -115,11 +96,5 @@ if(window.navigator.onLine){
     console.log('XCVB:',opHours);
    //fillRestaurantHoursHTML(opHours);
   });
-  
 }
 
-/////////////////////
-
-////ADD LOCALSTORAGE FOR REVIEW OFFLINE
-
-/////////////////
